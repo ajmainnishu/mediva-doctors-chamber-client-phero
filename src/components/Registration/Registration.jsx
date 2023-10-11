5c87b056e8d75530f2a3a8aa14f9af8a84a72906
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-11 18:07:23 +0600

    feat: Registration component added

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const Registration = () => {
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <h1 className="text-5xl font-bold">Registration now!</h1>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name="name" placeholder="name" className="input input-bordered" required />
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -23,10 +29,10 @@ const Login = () => {
                                 <input type="password" name="password" placeholder="password" className="input input-bordered" required />
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button className="btn btn-primary">Registration</button>
                             </div>
                         </form>
-                        <Link to={`/register`} className="text-center pb-5">Haven&apos;t an account ? Register</Link>
+                        <Link to={`/login`} className="text-center pb-5">Already have an account ? Login</Link>
                     </div>
                 </div>
             </div>
@@ -34,4 +40,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Registration;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, } from "react-router-dom";
 import App from "../App";
 import Login from "../components/Login/Login";
+import Registration from "../components/Registration/Registration";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
             {
                 path: 'login',
                 element: <Login />
+            },
+            {
+                path: 'register',
+                element:  <Registration />
             }
         ]
     }
