3d03f673f3f0d3336ce24def5a8b3e71c6a0c3d2
Ajmain Nishu <ajmainnishu@gmail.com>
2023-10-11 16:55:27 +0600

    feat: Footer component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Footer from './shared/Footer/Footer'
 import Navigation from './shared/Navigation/Navigation'
 
 function App() {
   return (
     <div>
       <Navigation />
+      <Footer />
     </div>
   )
 }
--- src/shared/Navigation/Navigation.jsx
+++ src/shared/Navigation/Navigation.jsx
@@ -14,7 +14,7 @@ const Navigation = () => {
                             <li><Link>Appointment</Link></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <a className="btn btn-ghost normal-case text-xl">Mediva</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
